a6335e1b17fc4f141840661f2c4ac11c47eb293f
cheleaux <nazareth.dnd@gmail.com>
2025-12-01 13:47:32 +0000

    In-Progress: adding image file storage

--- server/src/config.js
+++ server/src/config.js
@@ -4,5 +4,15 @@ module.exports = {
             tokenExpiryTime: '15m', // 15 minutes...
             relativeTimeNotationFormat: /^[1-9]\d*(s|m|h|d|w)$/,
         }
+    },
+    uploadThing: {
+        regionAlias: 'sea1',
+        urlSearchParamNames: {
+            UT_FILE_TYPE: 'x-ut-file-type',
+            UT_FILE_NAME: 'x-ut-file-name',
+            UT_FILE_SIZE: 'x-ut-file-size',
+            UT_IDENTIFIER: 'x-ut-identifier',
+            UR_CUSTOM_ID: 'x-ut-custom-id'
+        }
     }
 }
