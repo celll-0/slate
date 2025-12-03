963d308902a05ccea5e5251d6e9e205fe8a9e13e
Naz <nazareth.dnd@gmail.com>
2025-12-03 12:00:01 +0000

    refactor: improved asset auth controller response

--- server/src/controllers/auth/assetAuthParamController.js
+++ server/src/controllers/auth/assetAuthParamController.js
@@ -1,10 +1,10 @@
 const DigitalAssetFileStorage = require('../../services/digitalAssetService.js')
 
 function assetAuthParamController(req, res){
-    let fileDetails = req.body.fileDetails || {}
+    let fileDetails = req.body.fileDetails || {};
     const clientAuthParams = DigitalAssetFileStorage.getClientAuthParameters(fileDetails)
     res.set({"Access-Control-Allow-Origin" : "*"})
-    res.status(200).json(clientAuthParams);
+    res.status(200).json({token: clientAuthParams});
 }
 
 module.exports = assetAuthParamController
