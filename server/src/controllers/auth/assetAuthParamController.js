d02e03645b730fd530bc20a652c2b0900eddf8c3
Naz <nazareth.dnd@gmail.com>
2025-12-05 16:39:57 +0000

    small fix: corrected where controller gets user auth token

--- server/src/controllers/auth/assetAuthParamController.js
+++ server/src/controllers/auth/assetAuthParamController.js
@@ -2,7 +2,7 @@ const DigitalAssetFileStorage = require('../../services/digitalAssetService.js')
 
 function assetAuthParamController(req, res){
     let fileDetails = req.body.fileDetails || {};
-    const token = req.user.token;
+    const token = req.body.token;
     const clientAuthParams = DigitalAssetFileStorage.getClientAuthParameters(token, fileDetails)
     res.set({"Access-Control-Allow-Origin" : "*"})
     res.status(200).json({token: clientAuthParams});
